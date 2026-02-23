3e69fee7bf25314ec0990f6abe51b43d242fefa0
Jam1ll <jamilguzman202@gmail.com>
2026-02-22 23:57:10 -0400

    fixing email send with FormSubmit

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -14,7 +14,6 @@ export const ContactUs = () => {
           Estamos aquí para ayudarte.
         </p>
       </div>
-
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
           <div className="space-y-12">
